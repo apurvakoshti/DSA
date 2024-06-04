71ee382111e2e7eb4e661ec5a780680544c3d2f2
wadpractical <apurvakoshti03@gmail.com>
2024-06-04 11:05:29 +0530

    first commit

--- Strings/compare.java
+++ Strings/compare.java
@@ -1,8 +1,9 @@
 public class compare {
     public static void main(String args[]){
         String str1="abc";
-        String str2="abcd";
+        String str2="abc";
+        String str3= new String("abc");
         System.out.println(str1.equals(str2));
-        System.out.println(str1==str2);
+        System.out.println(str1==str3);
     }
 }
